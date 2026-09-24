084362a67dc94863bad53a857b78f6e0bbe621e1
continentalflyway-sys <continentalflyway@gmail.com>
2026-09-25 02:59:21 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-window.CF_CONFIG = {
-  SUPABASE_URL: 'https://thgahbuwnirlnlzcvdyx.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE',
-  PHONE: '+8801922422644',
-  EMAIL: 'continentalflyway@gmail.com',
-  FACEBOOK: 'https://facebook.com/ContinentalFlywayy'
-};
+window.CF_CONFIG={supabaseUrl:'https://thgahbuwnirlnlzcvdyx.supabase.co',supabaseKey:'sb_publishable_K7x_nuavUyO53DQg-yp8_g_maRkMURR'};
